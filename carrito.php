21f713d85380f3447c3b4d69c668532020fcedf2
J. Jesús Espinoza P <espinozajuan147@gmail.com>
2023-11-23 14:12:25 -0700

    Versiona final
    Tienda completo

---
+++ carrito.php
@@ -0,0 +1,155 @@
+<?php require_once "config/conexion.php";
+require_once "config/config.php";
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Carrito de Compras</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" /> -->
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <link href="assets/css/estilos.css" rel="stylesheet" />
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=<?php echo CLIENT_ID; ?>&locale=<?php echo LOCALE; ?>"></script>
+</head>
+
+<body>
+    <!-- Navigation-->
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./">AlFre Shops</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    </div>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">Carrito</h1>
+                <p class="lead fw-normal text-white-50 mb-0">Tus Productos Agregados.</p>
+            </div>
+        </div>
+    </header>
+    <section class="py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                    <th>Sub Total</th>
+                                </tr>
+                            </thead>
+                            <tbody id="tblCarrito">
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-5 ms-auto">
+                    <h4>Total a Pagar: <span id="total_pagar">0.00</span></h4>
+                    <div class="d-grid gap-2">
+                        <div id="paypal-button-container"></div>
+                        <button class="btn btn-warning" type="button" id="btnVaciar">Vaciar Carrito</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; AlFre Shops</p>
+        </div>
+    </footer>
+
+    <script>
+        let carritoData; // Variable para almacenar los datos del carrito
+
+        mostrarCarrito();
+
+        function mostrarCarrito() {
+            if (localStorage.getItem("productos") != null) {
+                let array = JSON.parse(localStorage.getItem('productos'));
+                if (array.length > 0) {
+                    $.ajax({
+                        url: 'ajax.php',
+                        type: 'POST',
+                        async: true,
+                        data: {
+                            action: 'buscar',
+                            data: array
+                        },
+                        success: function (response) {
+                            console.log(response);
+                            carritoData = JSON.parse(response);
+                            let html = '';
+                            carritoData.datos.forEach(element => {
+                                html += `
+                                <tr>
+                                    <td>${element.id}</td>
+                                    <td>${element.nombre}</td>
+                                    <td>${element.precio}</td>
+                                    <td>1</td>
+                                    <td>${element.precio}</td>
+                                </tr>
+                                `;
+                            });
+                            $('#tblCarrito').html(html);
+                            $('#total_pagar').text(carritoData.total);
+                            inicializarBotonPaypal();
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        }
+                    });
+                }
+            }
+        }
+
+        function inicializarBotonPaypal() {
+            paypal.Buttons({
+                createOrder: function (data, actions) {
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: carritoData.total
+                            }
+                        }]
+                    });
+                },
+                onApprove: function (data, actions) {
+                    return actions.order.capture().then(function (details) {
+                        alert('Transacción completada por ' + details.payer.name.given_name);
+                        window.location.href = 'http://localhost/alfre_shops/';
+                    });
+                }
+            }).render('#paypal-button-container');
+        }
+    </script>
+</body>
+
+</html>
---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define('CLIENT_ID', 'AXntB-ILm0TQ79bs6Y-p2dndLoPYWCYlGLHmwt9__7i0pPO5OAc5F2jaHfQlA9oyLo6lT02axVH84Uu2');
+define('CLIENT_SECRET', 'EO9KvxS0o-fGO4Ts8XezZEjnfH52VyfUUuqRwjXzFm5xxyaIayDwmMSvApnFzA7by5M0g3NpkTz117c4');
+define('LOCALE', 'es_ES');  // Otra configuración que puedas necesitar, dependiendo de tu aplicación
+?>
